559dc49502cf1280fd42217a835b5f2d7840365a
Glenn <gcrosby15@gmail.com>
2023-06-08 15:26:36 +0700

    Add get_videos() method to scrape data. This replaces the PlaylistScrape class. I cannot get PlaylistScrape to work on Colab. But a method will work. Passing tests

--- source/playlist_scraper.py
+++ source/playlist_scraper.py
@@ -1,8 +1,51 @@
 from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException
 import time  
 
 from source.config_reader import ConfigReader 
+
+
+
+# The PlaylistScraper class does not work in Colab for whatever reason.
+# ChromeDriver always crashes without any real reason. However the scraper
+# works find when ran in a method.
+
+
+def get_videos(playlist_url, pause_time=10):
+
+	options = Options()
+
+	options.add_argument('--headless')
+	options.add_argument('--no-sandbox')
+	#overcome limited resource problems
+	options.add_argument('--disable-dev-shm-usage')
+	options.add_argument("lang=en")
+	#open Browser in maximized mode
+	options.add_argument("start-maximized")
+	#disable infobars
+	options.add_argument("disable-infobars")
+	#disable extension
+	options.add_argument("--disable-extensions")
+	options.add_argument("--incognito")
+	options.add_argument("--disable-blink-features=AutomationControlled")
+
+	driver = webdriver.Chrome(options=options)
+
+	driver.get(playlist_url)
+	time.sleep(pause_time)
+	all_anchors = driver.find_elements(By.ID, 'video-title')
+	videos = [x.get_property('href') for x in all_anchors if x.get_property('id') == 'video-title']
+
+	print(driver.title)
+	driver.quit()
+
+	return videos 
+
+
+
+
 
 
 
--- source/tests/test_playlist_scraper.py
+++ source/tests/test_playlist_scraper.py
@@ -1,11 +1,13 @@
 import unittest
 
 from source.config_reader import ConfigReader
-from source.playlist_scraper import PlaylistScraper
+# from source.playlist_scraper import PlaylistScraper
+from source.playlist_scraper import get_videos
 
 
 
-class PlaylistScraperTest(unittest.TestCase):
+
+class GetVideosTest(unittest.TestCase):
 	'''
 	
 	There is not a great way to test this as the scraped data is dynamic.
@@ -22,22 +24,54 @@ class PlaylistScraperTest(unittest.TestCase):
 		self.all_playlists_urls = [url for playlist in all_playlists for url in playlist]
 
 
-
-	def test_initialization(self):
-		playlist_scraper = PlaylistScraper()
-		self.assertIsNone(playlist_scraper.videos)
-
-
 	def test_get_videos_test1(self):
-		playlist_scraper = PlaylistScraper()
-		playlist_scraper.get_videos(playlist_url=self.all_playlists_urls[0])
-		self.assertTrue(len(playlist_scraper.videos) > 0)
+		videos = get_videos(playlist_url=self.all_playlists_urls[0])
+		self.assertTrue(len(videos) > 0)
+
 
 
 	def test_get_videos_test2(self):
-		playlist_scraper = PlaylistScraper()
-		playlist_scraper.get_videos(playlist_url=self.all_playlists_urls[-1])
-		self.assertTrue(len(playlist_scraper.videos) > 0)
+		videos = get_videos(playlist_url=self.all_playlists_urls[-1])
+		self.assertTrue(len(videos) > 0)
+
+
+
+
+
+# This class does not work in Colab so not using it for now:
+# class PlaylistScraperTest(unittest.TestCase):
+# 	'''
+	
+# 	There is not a great way to test this as the scraped data is dynamic.
+# 	These tests provide basic "smoke test" in that the class doesent' blow
+# 	up when initialized and can scrape video urls.
+
+# 	'''	
+
+# 	def setUp(self):
+# 		# the playlist urls are in a dict object where the keys are categories
+# 		# and the values are list of playlist urls. This converts the
+# 		# dictionary object to a flat list:
+# 		all_playlists = [v for k, v in ConfigReader().playlists.items()]
+# 		self.all_playlists_urls = [url for playlist in all_playlists for url in playlist]
+
+
+
+# 	def test_initialization(self):
+# 		playlist_scraper = PlaylistScraper()
+# 		self.assertIsNone(playlist_scraper.videos)
+
+
+# 	def test_get_videos_test1(self):
+# 		playlist_scraper = PlaylistScraper()
+# 		playlist_scraper.get_videos(playlist_url=self.all_playlists_urls[0])
+# 		self.assertTrue(len(playlist_scraper.videos) > 0)
+
+
+# 	def test_get_videos_test2(self):
+# 		playlist_scraper = PlaylistScraper()
+# 		playlist_scraper.get_videos(playlist_url=self.all_playlists_urls[-1])
+# 		self.assertTrue(len(playlist_scraper.videos) > 0)
 
 
 
